f6e645c669dc5264832b3e6ffb601bd3d859a258
Thewilliamg <156461507+Thewilliamg@users.noreply.github.com>
2024-05-21 11:59:41 -0500

    feat: :sparkles: Se añade el DOM para pantalones
    se realiza en main.js por el momento

--- js/main.js
+++ js/main.js
@@ -1,11 +1,48 @@
-export const getAll = async() =>{
-    const url= await fetch("http://172.16.101.146:5999/");
+export const getPantalones = async() =>{
+    const url= await fetch("http://172.16.101.146:5999/pantalon");
     const data = await url.json();
     return data;
 }
 
-console.log(await getAll());
+export const getAbrigo = async() =>{
+    const url= await fetch("http://172.16.101.146:5999/abrigos");
+    const data = await url.json();
+    return data;
+}
+
+export const getCamiseta = async() =>{
+    const url= await fetch("http://172.16.101.146:5999/camiseta");
+    const data = await url.json();
+    return data;
+}
+
+
 
 // export const createCardAbrigo()=>{
-//     let 
-// }
+    //     let 
+    // }
+    // const createPantalones = document.querySelector("boxes");
+    // createPantalones.addEventListener("click", async (e) => {
+export const createPantalones = async()=>{
+    let boxes=document.querySelector(".boxes"); ;
+    console.log(!boxes.innerHTML);
+    if (!boxes.innerHTML) {
+        let data = await getPantalones()
+        let plantilla = "";
+        console.log(data);
+        data.forEach(val => {
+            plantilla += `
+                <div class="box" id=${val.id}>
+                    <img src =${val.imagen}
+                    <div class="info">
+                        <p>${val.nombre}</p>
+                        <b>${val.precio} </b>
+                        <button class="h_button">Agregar</button>
+                    </div>
+                </div>
+            `;})
+        boxes.innerHTML = plantilla;
+    }
+}
+await createPantalones();
+// })
