7a9a8ac52a73294590044c5a5a9c0bac0b5b96b3
Thewilliamg <156461507+Thewilliamg@users.noreply.github.com>
2024-05-21 11:16:05 -0500

    feat: :construction: Se intenta añadir el puerto node con json server

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-http://172.16.101.146:5999/
+export const getAll = async() =>{
+    const url= await fetch("http://172.16.101.146:5999/");
+    const data = await url.json();
+    return data;
+}
+
+console.log(await getAll());
+
+// export const createCardAbrigo()=>{
+//     let 
+// }
